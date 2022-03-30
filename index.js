486752b5b3abb021fa1de0140f11dce763d6fd8b
dandykong <37414102+dandykong@users.noreply.github.com>
2022-03-30 16:01:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-let canvas = null; // we'll keep it as a global object
-let gl = null; // it will store our context, and all the functions and constants that are needed to use it
+let canvas = null;
+let gl = null;
 
-function onResize() { // this function resizes our canvas in a way, that makes it fit the entire screen perfectly!
+function onResize() {
 	canvas.width = canvas.clientWidth * window.devicePixelRatio;
 	canvas.height = canvas.clientHeight * window.devicePixelRatio;
 }
 
-window.onresize = onResize; // sets the window's resize function to be the exact function we use for resizing our canvas
+window.onresize = onResize;
 
-function initWebGL2() {
-	canvas = document.createElement("canvas"); // creates a new canvas element ( <canvas></canvas> )
-	gl = canvas.getContext("webgl2"); // creates a WebGL2 context, using the canvas
-	if(!gl) { // if the gl DIDN'T create properly
-		alert("This browser does not support WebGL 2."); // alert the user about it
-		return; // go out of the function; stop this function
-	}
-	canvas.style = "position: absolute; width: 100%; height: 100%; left: 0; top: 0; right: 0; bottom: 0; margin: 0; z-index: -1;"; // we add a simple style to our canvas
-	document.body.appendChild(canvas); // appends/adds the canvas element to the document's body
-	onResize(); // resizes the canvas (it needs to be done, because otherwise it will not resize until you resize your window)
-	
-	gl.clearColor(1.0, 0.0, 0.0, 1.0); // specifies the clearing color to be read (using RGBA)
-	gl.clear(gl.COLOR_BUFFER_BIT); // clears the screen using a specified color
+function onFrame() {
+	gl.clearColor(0.3, 1.0, 0.4, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	window.requestAnimationFrame(onFrame);
 }
 
-initWebGL2(); // we call our init function, therefore initializing the application
+function initWebGL2() {
+	canvas = document.createElement("canvas");
+	gl = canvas.getContext("webgl2");
+	if(!gl) {
+		alert("This browser does not support WebGL 2.");
+		return;
+	}
+	canvas.style = "position: absolute; width: 100%; height: 100%; left: 0; top: 0; right: 0; bottom: 0; margin: 0; z-index: -1;";
+	document.body.appendChild(canvas);
+	onResize();
+	
+	gl.clearColor(1.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+initWebGL2();
