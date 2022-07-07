c30e1a2c1309ca6d66f24b7245bc1a1ea46504d8
dandykong <37414102+dandykong@users.noreply.github.com>
2022-07-07 09:25:08 -0500

    Another attempt at an active-button script

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -11,7 +11,13 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+            var navScript = document.createElement("script");
+            var navScriptCode = "document.getElementById(getActiveNav()).classList.add('nav_active');";
+            navScript.innerHTML = navScriptCode;
+            elmnt.appendChild(navScript); //Right now I'm only using w3-html-include for the navbar, so always inject this script.
+          }
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
